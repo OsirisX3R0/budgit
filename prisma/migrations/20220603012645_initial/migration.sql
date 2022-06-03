09230eb4083723256d806cbcef9577b8b67b470f
Richard Harris <richardharris916@gmail.com>
2022-06-02 21:05:32 -0500

    Initial migrations and database setup

---
+++ prisma/migrations/20220603012645_initial/migration.sql
@@ -0,0 +1,59 @@
+-- CreateEnum
+CREATE TYPE "schedule_type" AS ENUM ('onetime', 'weekly', 'biweekly', 'semimonthly', 'monthly');
+
+-- CreateEnum
+CREATE TYPE "biller_category" AS ENUM ('electricity', 'water', 'gas', 'phone', 'internet', 'auto');
+
+-- CreateTable
+CREATE TABLE "pay_schedule" (
+    "id" BIGSERIAL NOT NULL,
+    "type" "schedule_type" NOT NULL,
+    "amount" MONEY NOT NULL,
+    "day_of_week" INTEGER,
+    "day_of_month" INTEGER,
+    "next_occur" INTEGER,
+    "first_date" INTEGER,
+    "second_date" INTEGER,
+
+    CONSTRAINT "pay_schedule_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "biller" (
+    "id" BIGSERIAL NOT NULL,
+    "name" VARCHAR NOT NULL,
+    "category" "biller_category",
+    "day_of_month" TEXT NOT NULL,
+    "default_amount" INTEGER,
+
+    CONSTRAINT "biller_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "payday" (
+    "id" BIGSERIAL NOT NULL,
+    "day" INTEGER NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "amount" INTEGER NOT NULL,
+
+    CONSTRAINT "payday_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "payment" (
+    "id" BIGSERIAL NOT NULL,
+    "month" INTEGER NOT NULL,
+    "year" INTEGER NOT NULL,
+    "amount" MONEY NOT NULL,
+    "biller_id" BIGINT NOT NULL,
+    "payday_id" BIGINT NOT NULL,
+
+    CONSTRAINT "payment_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "payment" ADD CONSTRAINT "payment_biller_id_fkey" FOREIGN KEY ("biller_id") REFERENCES "biller"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "payment" ADD CONSTRAINT "payment_payday_id_fkey" FOREIGN KEY ("payday_id") REFERENCES "payday"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
