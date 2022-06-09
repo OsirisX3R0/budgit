32a3df43cf8c1b53f0f82b8670a142c0d41e6a43
Richard Harris <richardharris916@gmail.com>
2022-06-09 00:40:33 -0500

    Created SelectMenu core component

---
+++ src/components/core/SelectMenu.js
@@ -0,0 +1,14 @@
+import { Select, MenuItem } from "@mui/material";
+
+const SelectMenu = ({ items, ...selectProps }) => {
+  const menu = items.length
+    ? items.map((i) => (
+        <MenuItem key={i.value || i.text || i} value={i.value || i.text || i}>
+          {i.text || i.value || i}
+        </MenuItem>
+      ))
+    : null;
+  return <Select {...selectPropt}>{menu}</Select>;
+};
+
+export default SelectMenu;
