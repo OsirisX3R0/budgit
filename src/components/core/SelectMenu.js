b637d0c70162fd52452068750b1b464cfe346778
Richard Harris <richardharris916@gmail.com>
2022-06-19 08:24:47 -0500

    Updated SelectMenu to allow for objects as well as arrays

--- src/components/core/SelectMenu.js
+++ src/components/core/SelectMenu.js
@@ -1,14 +1,32 @@
 import { Select, MenuItem } from "@mui/material";
 
 const SelectMenu = ({ items, ...selectProps }) => {
-  const menu = items.length
-    ? items.map((i) => (
+  const menu = (items) => {
+    if (items.length) {
+      return items.map((i) => (
         <MenuItem key={i.value || i.text || i} value={i.value || i.text || i}>
           {i.text || i.value || i}
         </MenuItem>
-      ))
-    : null;
-  return <Select {...selectProps}>{menu}</Select>;
+      ));
+    }
+
+    if (typeof items === "object") {
+      let itemsArr = [];
+      for (let key in items) {
+        itemsArr.push(
+          <MenuItem key={key} value={key}>
+            {items[key]}
+          </MenuItem>
+        );
+      }
+
+      return itemsArr;
+    }
+
+    return null;
+  };
+
+  return <Select {...selectProps}>{menu(items)}</Select>;
 };
 
 export default SelectMenu;
